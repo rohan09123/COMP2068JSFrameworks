a2282f2dc3d6838719745821d4e68f02725a3dde
Rohan Vyas <61285222+rohan09123@users.noreply.github.com>
2024-02-12 08:58:58 -0500

    Application Created
    This is final version of my app.

--- ASSIGNMENT01/app.js
+++ ASSIGNMENT01/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const router = express.Router();
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'), (err) => {});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
